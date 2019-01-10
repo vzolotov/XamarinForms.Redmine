3d4b25c56b2680e8f89625851bcbd975b6dad7d6
Slava Zolotov <v.zolotov@outlook.com>
2019-01-10 13:53:46 +0300

    splashscreen

---
+++ Redmine.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿
+using Android.App;
+using Android.Support.V7.App;
+
+namespace Redmine.Droid
+{
+    [Activity(Label = "Redmine", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
