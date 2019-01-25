aa4d886a8a5bf208d8bf335be5a01de8de6f5581
Slava Zolotov <v.zolotov@outlook.com>
2019-01-25 15:08:39 +0300

    edit project

--- Redmine.ViewModels/EditProjectViewModel.cs
+++ Redmine.ViewModels/EditProjectViewModel.cs
@@ -1,7 +1,17 @@
+using System.Threading.Tasks;
+using ReactiveUI.Fody.Helpers;
+using Redmine.ViewModels.ItemViewModels;
+
 namespace Redmine.ViewModels
 {
     public class EditProjectViewModel : ViewModelBase
     {
-        
+        public override Task NavigateToAsync(object data)
+        {
+            Project = data as ProjectViewModel;
+            return base.NavigateToAsync(data);
+        }
+
+        [Reactive] ProjectViewModel Project {get;set;}
     }
 }
