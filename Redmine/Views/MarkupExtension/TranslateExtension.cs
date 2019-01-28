10c361fcc7eb5728feccd2972b19ead82c8f77a4
Slava Zolotov <v.zolotov@outlook.com>
2019-01-28 11:40:20 +0300

    add arrow as ios renderer

---
+++ Redmine/Views/MarkupExtension/TranslateExtension.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Redmine.Views.MarkupExtension
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        const string ResourceId = "Redmine.Localization.Resource";
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return null;
+            var resourceManager = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
+
+            return resourceManager.GetString(Text, CultureInfo.CurrentCulture);
+        }
+    }
+}
